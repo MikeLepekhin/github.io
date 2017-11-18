8f9538234c1e1d95d518de49eb36f82b7e8ed059
mike <mike@localhost.localdomain>
2017-11-18 21:38:19 +0300

    dsdasdfsdfsdfgs

--- app.js
+++ app.js
@@ -13,6 +13,5 @@ db.addEventListener("click", function() {
 
 ab.addEventListener("click", function() {
   var nwText = document.getElementById('add_task_input').value;
-  alert(nwText);
- // document.getElementById('root').innerHTML += '<ul><li class="bad_li"> <span>' + nwText + '</span></li></ul>');
+  document.getElementById('root').innerHTML += '<ul><li class="bad_li"> <span>' + nwText + '</span></li></ul>';
 });
