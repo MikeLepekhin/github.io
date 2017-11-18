65f0aae3cf4988a1a39df17e5e5ccdb25e0426c8
mike <mike@localhost.localdomain>
2017-11-18 23:40:28 +0300

    success

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   $('#root').append('<ul><li><span>Сделать задание #3 по web-программированию</span> <button class="remove">Удалить</button> </li></ul>');  
   $('#root').append('<input id="add_task_input"></input><button id="add_task"></button>');
   
-  $('#adder').click(function() {
+  $('#add_task').click(function() {
     $('#root ul').append("<li><span> </span> <button class='remove'>Удалить</button> </li>");
     $('#root ul li:last-child span').text($("#add_task_input").val());
   });
