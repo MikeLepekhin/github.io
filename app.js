bcc38802ab6021ae071c27c829826ac648646809
mike <mike@localhost.localdomain>
2017-11-18 21:04:25 +0300

    fdfsdfsdfds

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
 alert('Hello, dear friend!');
 var rt = document.getElementById('root');
-rt.innerHtml = '<ul><li> <span>Сделать задание #3 по web-программированию</span> <button id = "del_btn">Удалить</button> </li></ul>';
+rt.innerHTML = '<ul><li> <span>Сделать задание #3 по web-программированию</span> <button id = "del_btn">Удалить</button> </li></ul>';
+rt.innerHTML += '<input id="add_task_input"></input><button id="add_task"><\button>';
