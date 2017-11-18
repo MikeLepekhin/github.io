032a03c8095c1f81a2254105e5d2cd088871981e
mike <mike@localhost.localdomain>
2017-11-18 20:46:41 +0300

    sdfsdfds

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
 alert('Hello, dear friend!');
-var rt = document.getElementById('root');
+/*var rt = document.getElementById('root');
 rt.innerHtml = '<ul><li> <span>Сделать задание #3 по web-программированию</span> <button id = "del_btn">Удалить</button> </li></ul>';
+*/
