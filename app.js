1c396dab233b5d6119580327a513dab2ba9b361a
mike <mike@localhost.localdomain>
2017-11-18 20:40:13 +0300

    kek

--- app.js
+++ app.js
@@ -1,2 +1,3 @@
+alert('Hello, dear friend!');
 var rt = document.getElementById('root');
 rt.innerHtml += '<ul><li> <span>Сделать задание #3 по web-программированию</span> <button>Удалить</button> </li></ul>';
