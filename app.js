e74a3033a40c82b48247cd2f3498ba93efdf7d3c
mike <mike@localhost.localdomain>
2017-11-18 20:42:08 +0300

    kekkk

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 alert('Hello, dear friend!');
 var rt = document.getElementById('root');
-rt.innerHtml += '<ul><li> <span>Сделать задание #3 по web-программированию</span> <button>Удалить</button> </li></ul>';
+rt.innerHtml += '<ul><li> <span>Сделать задание #3 по web-программированию</span> <button id = "del_btn">Удалить</button> </li></ul>';
