36e273df4382b751523578b84adeac75b120aec3
mike <mike@localhost.localdomain>
2017-11-18 22:34:48 +0300

    asdksadksadk

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
 $(document).ready(function() {
   $('#root').append('<ul><li><span>Сделать задание #3 по web-программированию</span> <button id="deleter">Удалить</button> </li></ul>');  
+  $('#root').append('<input id="add_task_input"></input><button id="adder"></button>');
+  
+  $('#adder').click(function() {
+    $('#root ul').append("<li><span> </span> <button id='deleter'>Удалить</button> </li>");
+    $('#root ul li span').text($("#add_task_input").val());
+  });
+  
+  $('#deleter').click(function() {
+    $(this).parent().remove();
+  });
 });
