712f1d6999396b434d4c4d68d6e7636a0e21fd28
mike <mike@localhost.localdomain>
2017-11-18 23:33:04 +0300

    dasdsafdlsa

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 $(document).ready(function() {
-  $('#root').append('<ul><li><span>Сделать задание #3 по web-программированию</span> <button id="deleter">Удалить</button> </li></ul>');  
+  $('#root').append('<ul><li><span>Сделать задание #3 по web-программированию</span> <button class="remove">Удалить</button> </li></ul>');  
   $('#root').append('<input id="add_task_input"></input><button id="adder"></button>');
   
   $('#adder').click(function() {
-    $('#root ul').append("<li><span> </span> <button id='deleter'>Удалить</button> </li>");
+    $('#root ul').append("<li><span> </span> <button class='remove'>Удалить</button> </li>");
     $('#root ul li:last-child span').text($("#add_task_input").val());
   });
   
-  $('#root ul li button').click(function() {
+  $('html').on("click", ".remove", function() {
     $(this).parent().remove();
   });
 });
