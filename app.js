5e7dd9b1afeecc22927e6605711923b45fbda34a
mike <mike@localhost.localdomain>
2017-11-18 21:18:18 +0300

    fsdsdgsdgds

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 alert('Hello, dear friend!');
 var rt = document.getElementById('root');
 rt.innerHTML = '<ul><li id="bad_li"> <span>Сделать задание #3 по web-программированию</span> <button id = "del_btn">Удалить</button> </li></ul>';
-rt.innerHTML += '<input id="add_task_input"></input><button id="add_task"></button>';
+rt.innerHTML += '<input id="add_task_input"></input><button id="add_task">Добавить</button>';
 
 var db = document.getElementById('del_btn');
 var ab = document.getElementById('add_task');
@@ -9,4 +9,9 @@ var ab = document.getElementById('add_task');
 db.addEventListener("click", function() {
   var elem = document.getElementById('bad_li');
   elem.parentNode.removeChild(elem);  
+});
+
+ab.addEventListener("click", function() {
+  var nwText = document.getElementById('add_task_input').text;
+  alert(nwText);
 });
